42030bfae57a4a7d90f52a9fb7b4901cdc5af50a
kanato1221 <kanatonton2@gmail.com>
2025-11-30 13:48:36 +0900

    feat: クイズ機能

--- word/QuizView.swift
+++ word/QuizView.swift
@@ -1,13 +1,45 @@
 import SwiftUI
+import SwiftData
 
 struct QuizView: View{
+    @Environment(\.modelContext) var context
+    @Query var words: [Word]
+    @State var index = 0
+    @State var showAnser = false
+    
     var body: some View{
-        VStack{
-            Text("Quiz")
+        VStack(spacing: 24){
+            Spacer()
+            
+            if words.isEmpty{
+                Text("単語が登録されていません")
+            }else{
+                Text(words[index].english)
+                    .font(.largeTitle)
+                
+                Text(showAnser ? words[index].japanese: "ー")
+                    .font(.largeTitle)
+                
+                Button {
+                    showAnser.toggle()
+                }label:{
+                    Text(showAnser ? "解答を非表示":"解答を表示")
+                }
+                .font(.title)
+                .foregroundStyle(.white)
+                .padding(.vertical ,12)
+                .frame(maxWidth: .infinity)
+                .background(Color.orange)
+                .clipShape(.capsule)
+            }
+            
+            Spacer()
         }
+        .padding(.horizontal, 32)
     }
 }
 
 #Preview {
     QuizView()
+        .modelContainer(wordContainer)
 }
