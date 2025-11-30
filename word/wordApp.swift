6714a2912366a1e065218f8ed7aaae3a5aa16612
kanato1221 <kanatonton2@gmail.com>
2025-11-30 11:13:24 +0900

    Initial Commit

---
+++ word/wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordApp.swift
+//  word
+//
+//  Created by 松井奏人 on 2025/11/30.
+//
+
+import SwiftUI
+
+@main
+struct wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
